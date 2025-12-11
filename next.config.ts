c84d1e1fb4d5e78359226a8e1918385e308466a9
Rohmat Choiruly Anwar <rohmatchoirulyanwar@gmail.com>
2025-12-11 10:37:22 +0700

    feat: externalize `jsdom` and `isomorphic-dompurify` in Next.js server config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  serverExternalPackages: ["jsdom", "isomorphic-dompurify"],
 };
 
 export default nextConfig;
